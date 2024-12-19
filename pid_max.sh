e06622c9034674a01e7e1dfce4e20ef0d0e78276
meurtphy <erwanmpn5@gmail.com>
2024-12-19 10:36:50 +0100

    Conserver uniquement simple_shell.c

--- pid_max.sh
+++
@@ -1,3 +0,0 @@
-pid_max=$(cat /proc/sys/kernel/pid_max)
-
-echo "La valeur maximale d'un PID est : $pid_max"
--- split_string.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPLIT_STRING_H
-#define SPLIT_STRING_H
-
-char **split_string(char *str);
-
-#endif
