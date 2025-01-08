37f4e0b6e5c82ffc4b9a098743835f9d7b7b807a
meurtphy <erwanmpn5@gmail.com>
2025-01-08 22:54:36 +0100

    atoirm

--- handle_exit.c
+++ handle_exit.c
@@ -1,4 +1,23 @@
 #include "shell.h"
+
+int string_to_int(char *str)
+{
+int i = 0, num = 0;
+
+if (!str || str[0] == '\0')
+return (-1);
+
+while (str[i])
+{
+if (str[i] < '0' || str[i] > '9')
+return (-1);
+num = num * 10 + (str[i] - '0');
+i++;
+}
+
+return (num);
+}
+
 int handle_exit(char *command)
 {
 if (strncmp(command, "exit", 4) == 0 && (command[4] == '\0' || command[4] == ' '))
@@ -6,7 +25,13 @@ if (strncmp(command, "exit", 4) == 0 && (command[4] == '\0' || command[4] == ' '
 if (command[4] != '\0')
 {
 char *arg = command + 5;
-int status = atoi(arg);
+int status = string_to_int(arg);
+
+if (status == -1)
+{
+fprintf(stderr, "exit: Invalid number: %s\n", arg);
+return (0);
+}
 exit(status);
 }
 exit(0);
